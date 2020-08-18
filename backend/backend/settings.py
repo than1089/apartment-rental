00169227b77e145357bb0b3e9691976b13c08976
Ngo Hoai Than <than1089@gmail.com>
2020-08-19 00:27:04 +0700

    Update setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -192,7 +192,9 @@ DEFAULT_FROM_EMAIL = 'than1089@gmail.com'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+if AWS_ACCESS_KEY_ID:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_QUERYSTRING_AUTH = False
@@ -212,3 +214,4 @@ REST_USE_JWT = True
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
